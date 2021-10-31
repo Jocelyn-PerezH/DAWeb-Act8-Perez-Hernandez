73771565d8c7d31aa46b062cec95ebdd69031e4d
Jocelyn-PerezH <91572904+Jocelyn-PerezH@users.noreply.github.com>
2021-10-31 17:47:00 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>01 Perez</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logodl.png" height=85px width=270px></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="empleado.php">Empleado</a></li>
+					<li><a href="articulos.php">Articulos</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
